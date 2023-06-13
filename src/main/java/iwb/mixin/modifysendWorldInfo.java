ee18d6eebc9f4c2fe82277a1f5056c00e8502a5d
Daniel Jang <minhyukjang@gmail.com>
2023-06-13 21:28:44 +0900

    Add a Mixin to mod `PlayerManager`'s `sendWorldInfo()`
    One of the things that a server does after a player collides with a portal block is that it sends information about the common (Overworld) world border (its center, its size, etc.) so that the client can reconstruct it in its newly created `ClientWorld`. This poses a dire problem when we wish to let each dimension's world border be adjusted seperately, however. This Mixin makes the server send the "correct"/corresponding `ServerWorld`'s world border information instead of that of the Overworld.

---
+++ src/main/java/iwb/mixin/modifysendWorldInfo.java
@@ -0,0 +1,21 @@
+package iwb.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.server.PlayerManager;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.At;
+import net.minecraft.world.border.WorldBorder;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+@ Mixin (PlayerManager.class) class modifysendWorldInfo {
+  @ Redirect (
+    method = "sendWorldInfo",
+    at = @ At (
+      value = "INVOKE",
+      target = "Lnet/minecraft/server/world/ServerWorld;getWorldBorder()Lnet/minecraft/world/border/WorldBorder;"
+    )
+  ) WorldBorder moddedgetWorldBorder(ServerWorld overworld, ServerPlayerEntity player, ServerWorld world) {
+    return world.getWorldBorder();
+  }
+}
