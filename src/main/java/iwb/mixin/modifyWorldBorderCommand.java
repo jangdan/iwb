cb5eb458232b0a4913a31e06b080e22909ea7dc2
Daniel Jang <minhyukjang@gmail.com>
2023-06-14 00:29:11 +0900

    Add a Mixin that deals with all the other cases of `getOverworld().getWorldBorder()`

---
+++ src/main/java/iwb/mixin/modifyWorldBorderCommand.java
@@ -0,0 +1,21 @@
+package iwb.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.server.command.WorldBorderCommand;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.At;
+import net.minecraft.world.border.WorldBorder;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.command.ServerCommandSource;
+
+@ Mixin (WorldBorderCommand.class) class modifyWorldBorderCommand {
+  @ Redirect (
+    method = {"executeBuffer", "executeDamage", "executeWarningTime", "executeWarningDistance", "executeGet", "executeCenter", "executeSet"},
+    at = @ At (
+      value = "INVOKE",
+      target = "Lnet/minecraft/server/world/ServerWorld;getWorldBorder()Lnet/minecraft/world/border/WorldBorder;"
+    )
+  ) private static WorldBorder moddedgetWorldBorder(ServerWorld overworld, ServerCommandSource source) {
+    return source.getWorld().getWorldBorder();
+  }
+}
